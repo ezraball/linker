2f43d6483a4aaa82ec65df2db679d206dd4f44e2
Ezra Ball <ezraball@gmail.com>
2017-02-20 07:12:19 -0500

    don’t blow away sqlite database between deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ set :ssh_options, {
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
